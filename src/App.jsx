ae45ada73f49092f9d8a9e7be557636ae2fe1b1f
justinnonso05 <chinonsoali2005@gmail.com>
2025-06-03 20:24:38 +0100

    Added rerouting for / page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // src/App.jsx
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Dashboard from './pages/dashboard/dashboard';
 import Auth from './pages/auth/auth';
 import ProtectedRoute from './ProtectedRoute';
@@ -10,6 +10,7 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route path='/' element={<Navigate to="/auth" replace/>} />
         <Route path="/auth" element={<Auth />} />
         <Route path="/:shortName" element={<DuesPayPaymentFlow />} />
         <Route path="/dashboard" element={
